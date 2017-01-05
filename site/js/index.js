1125cf4c8e59d18907d92b69f076c16e7aeae486
Yogender <ybisht1997@gmail.com>
2017-01-05 21:25:28 +0530

    to top button color fixed

--- site/js/index.js
+++ site/js/index.js
@@ -1,4 +1,39 @@
-function helloWorld() {
-    var listItem = document.getElementById(sidebar-link1);
-    document.write("Yogi");
+//window.onscroll = function() {chVisibility()};
+function chVisibility(el) {
+	//console.log(document.body.scrollTop);
+	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+		var navBar = document.getElementById("stickyNavBar");
+		navBar.classList.add("showStickyNavBar");
+		// navBar.style.transition = "visibility 1s";
+		// navBar.style.display = "block";
+	}
+	else {
+		document.getElementById("stickyNavBar").classList.remove("showStickyNavBar");
+	}
+
+	/*################### Move To Top Button ################*/
+    var ael = document.getElementById("toTop");
+    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    	// ael.style.visibility = "visible";
+    	// animToTop(ael);
+    	ael.classList.add("animtoTop");
+    }
+    else {
+    	// ael.style.visibility = "hidden";
+    	ael.classList.remove("animtoTop");
+    }
+
 }
+// function animToTop(el) {
+// 	var id = setInterval(frame,2);
+// 	var op = 0;
+// 	function frame() {
+// 		if (op == 10) {
+// 			clearInterval(id);
+// 		}
+// 		else {
+// 			el.style.opacity = op * 0.1;
+// 			op ++;  
+// 		}
+// 	}
+// }
