55f7d48d18e35fcafc5d78ed2c0c674e5cf0ff2e
Yogender <ybisht1997@gmail.com>
2017-01-11 02:03:04 +0530

    lots of improvement
    code reduced using jQuery
    added animated scrolling to top
    default margin-top of direct h1 child of every div element removed

--- site/js/index.js
+++ site/js/index.js
@@ -1,19 +1,48 @@
 //window.onscroll = function() {chVisibility()};
-function chVisibility(el) {
+$(function() {
+	$(window).scroll(function() {
+		if($(this).scrollTop() > 150) {
+			$("#stickyNavBar").fadeIn(300);
+			$("#scrollToTop").fadeIn(200);
+		}
+		else {
+			$("#stickyNavBar").fadeOut(400);
+		 	$("#scrollToTop").fadeOut(200);
+		}
+	});
+
+	$('#scrollToTop').click(function(event){
+		event.preventDefault();
+		var win = $('html, body');
+		$(win).stop().animate({scrollTop : $(win).offset().top},300);
+		// return false;
+	});
+	// $("#toTop").click(function(event) {
+	// 	var target = $( $(this).attr('href') );
+
+ //    if( target.length ) {
+ //        event.preventDefault();
+ //        $('html, body').animate({
+ //            scrollTop: target.offset().top
+ //        }, 500);
+ //    }
+	// });
+});
+// function chVisibility(el) {
 	//console.log(document.body.scrollTop);
-	if ($(document).scrollTop() > 150 || document.documentElement.scrollTop > 150) {
+	// if ($(document).scrollTop() > 150 || document.documentElement.scrollTop > 150) {
 		// var navBar = document.getElementById("stickyNavBar");
 		// navBar.classList.add("showStickyNavBar");
-		$("#stickyNavBar").fadeIn(300);
-		$("#toTop").fadeIn(200);
+		// $("#stickyNavBar").fadeIn(300);
+		// $("#toTop").fadeIn(200);
 		// navBar.style.transition = "visibility 1s";
 		// navBar.style.display = "block";
-	}
-	else {
-		document.getElementById("stickyNavBar").classList.remove("showStickyNavBar");
-		$("#stickyNavBar").fadeOut(400);
-		$("#toTop").fadeOut(200);
-	}
+	// }
+	// else {
+	// 	document.getElementById("stickyNavBar").classList.remove("showStickyNavBar");
+	// 	$("#stickyNavBar").fadeOut(400);
+	// 	$("#toTop").fadeOut(200);
+	// }
 
 	// /*################### Move To Top Button ################*/
  //    var ael = document.getElementById("toTop");
@@ -27,7 +56,7 @@ function chVisibility(el) {
  //    	ael.classList.remove("animtoTop");
  //    }
 
-}
+// }
 // function animToTop(el) {
 // 	var id = setInterval(frame,2);
 // 	var op = 0;
